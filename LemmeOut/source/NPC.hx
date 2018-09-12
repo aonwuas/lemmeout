d3b6e8f6b8f2922d8877fcc49064924eed2bcad4
Anthony <ieatbeens@gmail.com>
2018-09-12 15:40:58 -0400

    Began NPC class

---
+++ LemmeOut/source/NPC.hx
@@ -0,0 +1,19 @@
+package;
+import flixel.FlxSprite;
+import flixel.FlxObject;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+
+/**
+ * ...
+ * @author Anthony Ben Jerry Rachel Steven
+ */
+class NPC extends FlxSprite
+{
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		
+	}
+	
+}
