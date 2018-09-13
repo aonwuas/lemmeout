ea1a37fc601a866b3153981849b0155565988b13
Jerry <jerry.s.jacobs@gmail.com>
2018-09-13 17:06:17 -0400

    Uploading blob with controls

--- LemmeOut/source/PlayState.hx
+++ LemmeOut/source/PlayState.hx
@@ -1,30 +1,23 @@
 package;
 
 import flixel.FlxState;
-import flixel.FlxG;
-import flixel.ui.FlxButton;
 
 class PlayState extends FlxState
 {
-var _playButton: FlxButton;
+
 var _player:Player;
 
 	override public function create():Void
 	{
-		_player = new Player(20,20);
+		_player = new Player();
 		add(_player);
+		_player.screenCenter();
 		super.create();
-        _playButton = new FlxButton(0,0,"Play!",clickPlay);
-        _playButton.screenCenter();
-        add(_playButton);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
+		_player.movement();
 		super.update(elapsed);
 	}
-
-    function clickPlay():Void{
-        FlxG.switchState(new PlayState());
-    }
 }
