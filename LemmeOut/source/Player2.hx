cc7b21b5473299fc6b5157aacccff82fcfea0293
Jerry <jerry.s.jacobs@gmail.com>
2018-09-17 08:03:59 -0400

    Merged player into player2

--- LemmeOut/source/Player2.hx
+++ LemmeOut/source/Player2.hx
@@ -8,9 +8,10 @@
  class Player2 extends Character
  {
     public var spd:Float = 200;
-	private var player_graphic:String = "assets/images/GD1_BlobSpriteSheetx100.png";
-    private var player_width:Int = 100;
-	private var player_height:Int = 100;
+	public var controlled:Bool = true;
+	private var player_graphic:String = "assets/images/GD1_blobMasterSheet.png";
+    private var player_width:Int = 32;
+	private var player_height:Int = 32;
     
     public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset)
     {
@@ -22,25 +23,55 @@
 	override public function setFlxSprite(image_string:String, is_animated:Bool = false, width:Int, height:Int):Void 
 	{
 		super.setFlxSprite(image_string, is_animated, width, height);
-		addAnim("front", [0, 0, 1, 1, 0, 2, 1, 3], 5, true);
-		addAnim("back", [4,4,5,5], 5, true);
-		addAnim("side", [6,6,7,7], 5, true);
-		playAnim("front");
+		addAnim("front", [0,0,1,1,0,0,1,1,0,2,1,3], 7, true);
+		addAnim("back", [4,4,5,5], 7, true);
+		addAnim("side", [6,6,7,7], 7, true);
+		addAnim("front_grey", [8,8,9,9,8,8,9,9,8,10,9,11], 3, true);
+		addAnim("back_grey", [12,12,13,13], 3, true);
+		addAnim("side_grey", [14,14,15,15], 3, true);
+		playAnim("front"); //starting animation
 	}
 
      public function movement():Void
     {
-        if (FlxG.keys.anyPressed([UP, DOWN, LEFT, RIGHT, W, A, S, D])){
-			switch(FlxG.keys.firstPressed()){
-				case FlxKey.UP, FlxKey.W:
-					move(FlxObject.UP);
-				case FlxKey.DOWN, FlxKey.S:
-					move(FlxObject.DOWN);
-				case FlxKey.LEFT, FlxKey.A:
-					move(FlxObject.LEFT);
-				case FlxKey.RIGHT, FlxKey.D:
-					move(FlxObject.RIGHT);
+		if (controlled)
+		{
+			switch (flxsprite.facing){ //turn green when space pressed
+                case FlxObject.LEFT, FlxObject.RIGHT:
+                    playAnim("side");
+                case FlxObject.UP:
+                    playAnim("back");
+                case FlxObject.DOWN:
+                    playAnim("front");
+            }
+
+			if (FlxG.keys.justPressed.E) {controlled = false;} //shoot beam
+
+			if (FlxG.keys.anyPressed([UP, DOWN, LEFT, RIGHT, W, A, S, D])){
+				switch(FlxG.keys.firstPressed()){
+					case FlxKey.UP, FlxKey.W:
+						move(FlxObject.UP);
+					case FlxKey.DOWN, FlxKey.S:
+						move(FlxObject.DOWN);
+					case FlxKey.LEFT, FlxKey.A:
+						move(FlxObject.LEFT);
+					case FlxKey.RIGHT, FlxKey.D:
+						move(FlxObject.RIGHT);
+				}
 			}
+		}
+		else
+		{
+            if (FlxG.keys.justPressed.SPACE) {controlled = true;} //control
+
+            switch (flxsprite.facing){ //not controlled animation
+                case FlxObject.LEFT, FlxObject.RIGHT:
+                	playAnim("side_grey");
+                case FlxObject.UP:
+                    playAnim("back_grey");
+                case FlxObject.DOWN:
+                    playAnim("front_grey");
+            }
 		}
     }
  }
