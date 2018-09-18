5c0c6e14ea59562d98a36334e084ea4c7bc6e2c3
Stephen <frombs@rpi.edu>
2018-09-18 16:26:01 -0400

    Loaded background (no collision)

--- LemmeOut/export/neko/haxe/ApplicationMain.hx
+++ LemmeOut/export/neko/haxe/ApplicationMain.hx
@@ -38,7 +38,7 @@ import haxe.macro.Expr;
 		
 		var config = {
 			
-			build: "19",
+			build: "21",
 			company: "HaxeFlixel",
 			file: "LemmeOut",
 			fps: 60,
@@ -110,7 +110,7 @@ import haxe.macro.Expr;
 		#if (lime >= "7.0.0")
 		ManifestResources.init (config);
 		
-		app.meta["build"] = "19";
+		app.meta["build"] = "21";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "LemmeOut";
 		app.meta["name"] = "LemmeOut";
--- LemmeOut/source/PlayState.hx
+++ LemmeOut/source/PlayState.hx
@@ -1,17 +1,47 @@
 package;
 
+import flixel.FlxObject;
 import flixel.FlxState;
 import flixel.FlxSprite;
+import flixel.addons.editors.tiled.*;
 import flixel.group.FlxGroup;
-//import flixel.FlxObject;
+import flixel.system.debug.interaction.tools.Tool;
+import flixel.tile.FlxTilemap;
+import flixel.tile.FlxBaseTilemap;
+import openfl.Assets;
 class PlayState extends FlxState
 {
 
 var _player:Player;
 public var playerBullets:FlxTypedGroup<FlxSprite>;
+public var walls:FlxObject;
+public var exit:FlxSprite;
 
 	override public function create():Void
 	{
+		/*var tileMap:FlxTilemap = new FlxTilemap();
+        var mapData:String = Assets.getText("assets/data/level1.csv");
+        var mapTilePath:String = "assets/images/Levels/FloorAndWallTiles.tsx";
+        tileMap.loadMap(mapData, mapTilePath, 16, 16);
+        add(tileMap);*/
+		var _map:TiledMap;
+		var _mWalls:FlxTilemap;
+		_map = new TiledMap(AssetPaths.test2__tmx);
+		_mWalls = new FlxTilemap();
+		_mWalls.loadMapFromArray(cast(_map.getLayer("floor"), TiledTileLayer).tileArray, _map.width, _map.height, AssetPaths.background__png, _map.tileWidth, _map.tileHeight, FlxTilemapAutoTiling.OFF, 1, 1, 3);
+		_mWalls.follow();
+		_mWalls.setTileProperties(0, FlxObject.ANY);
+		_mWalls.setTileProperties(1, FlxObject.NONE);
+		_mWalls.setTileProperties(2, FlxObject.NONE);
+		_mWalls.setTileProperties(3, FlxObject.NONE);
+		_mWalls.setTileProperties(4, FlxObject.NONE);
+		_mWalls.setTileProperties(5, FlxObject.NONE);
+		_mWalls.setTileProperties(6, FlxObject.NONE);
+		_mWalls.setTileProperties(7, FlxObject.NONE);
+		_mWalls.setTileProperties(8, FlxObject.NONE);
+		_mWalls.setTileProperties(9, FlxObject.NONE);
+		add(_mWalls);
+ 
 		//setup bullets
 		var numPlayerBullets:Int = 10;
 		playerBullets = new FlxTypedGroup(numPlayerBullets);
@@ -30,9 +60,29 @@ public var playerBullets:FlxTypedGroup<FlxSprite>;
 		_player = new Player();
 		Character.addToPlayState(this, _player);
 		_player.screenCenter();
+		/*
+		var tmpMap:TiledObjectLayer = cast _map.getLayer("GameObjects");
+		for (e in tmpMap.objects)
+		{
+			placeEntities(e.type, e.xmlData.x);
+		}
+
+*/
 		super.create();
 	}
-
+	/*
+	 function placeEntities(entityName:String, entityData:Xml):Void
+	 {
+     var x:Int = Std.parseInt(entityData.get("x"));
+     var y:Int = Std.parseInt(entityData.get("y"));
+     if (entityName == "player")
+		{
+         _player.x = x;
+         _player.y = y;
+		}
+	 }
+	*/
+	 
 	override public function update(elapsed:Float):Void
 	{
 		_player.movement();
