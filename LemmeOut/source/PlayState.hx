c59699b15049f63f746527885fa55170830c6850
Ben Krowitz <krowbar179@gmail.com>
2018-09-18 15:39:19 -0400

    Fixed merge conflict with TileMap

--- LemmeOut/source/PlayState.hx
+++ LemmeOut/source/PlayState.hx
@@ -8,15 +8,35 @@ import flixel.util.FlxCollision;
 class PlayState extends FlxState
 {
 
+var _map:TiledMap;
+var _mWalls:FlxTilemap;
 var _player:Player;
-public var _bullet:FlxSprite;
+
+public var playerBullets:FlxTypedGroup<FlxSprite>;
 
 	override public function create():Void
 	{
-		//setup bullet
-		_bullet = new FlxSprite(0,0);
-		_bullet.loadGraphic("assets/images/GD1_mindblip.png",false,16,16);
-		add(_bullet);
+		//setup bullets
+		var numPlayerBullets:Int = 10;
+		playerBullets = new FlxTypedGroup(numPlayerBullets);
+		var sprite:FlxSprite;
+		for (i in 0...numPlayerBullets) //recycle bullets
+		{
+			//instantiate a new sprite offscreen
+			sprite = new FlxSprite(0,0);
+			sprite.loadGraphic("assets/images/GD1_mindblip.png",false,16,16);
+			sprite.exists = false;
+			playerBullets.add(sprite);
+		}
+		add(playerBullets);
+		//setup map
+		_map = new TiledMap(AssetPaths.room_001__tmx);
+		_mWalls = new FlxTilemap();
+		_mWalls.loadMapFromArray(cast(_map.getLayer("walls"), TiledTileLayer).tileArray, _map.width, _map.height, AssetPaths.tiles__png, _map.tileWidth, _map.tileHeight, FlxTilemapAutoTiling.OFF, 1, 1, 3);
+		_mWalls.follow();
+		_mWalls.setTileProperties(2, FlxObject.NONE);
+		_mWalls.setTileProperties(3, FlxObject.ANY);
+		add(_mWalls);
 
 		//setup player
 		_player = new Player();
