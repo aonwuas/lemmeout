52d8ca192a6365863ad6391f69312474473a5329
Ben Krowitz <krowbar179@gmail.com>
2018-09-19 23:33:26 -0400

    Remade test map
    I swear to God if git somehow eats this again I will eat git, I mean all of it

--- LemmeOut/assets/data/testTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="testTiles" tilewidth="32" tileheight="32" tilecount="10" columns="10">
- <image source="../images/background.png" width="320" height="32"/>
-</tileset>
