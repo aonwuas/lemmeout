c698d7b25421453ecdf4b2a22a66b788030f602d
Stephen <frombs@rpi.edu>
2018-09-21 13:59:14 -0400

    final change

--- LemmeOut/export/neko/haxe/ApplicationMain.hx
+++ LemmeOut/export/neko/haxe/ApplicationMain.hx
@@ -38,7 +38,7 @@ import haxe.macro.Expr;
 		
 		var config = {
 			
-			build: "126",
+			build: "127",
 			company: "HaxeFlixel",
 			file: "LemmeOut",
 			fps: 60,
@@ -110,7 +110,7 @@ import haxe.macro.Expr;
 		#if (lime >= "7.0.0")
 		ManifestResources.init (config);
 		
-		app.meta["build"] = "126";
+		app.meta["build"] = "127";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "LemmeOut";
 		app.meta["name"] = "LemmeOut";
